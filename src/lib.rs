ba42d6689bd00c099eefea3c0c8e79cdeb1e3b16
Matthew Michelotti <michelotti.matthew@gmail.com>
2016-08-28 19:32:22 -0500

    working on documentation...

--- src/lib.rs
+++ src/lib.rs
@@ -12,12 +12,66 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//! This crate contains floating point types that panic if they are set
+//! to an illegal value, such as NaN.
+//!
+//! The name "Noisy Float" comes from
+//! the terms "quiet NaN" and "signaling NaN"; "signaling" was too long
+//! to put in a struct/crate name, so "noisy" is used instead, being the opposite
+//! of "quiet."
+//!
+//! The standard types defined in `noisy_float::types` follow the principle
+//! demonstrated by Rust's handling of integer overflow:
+//! a bad arithmetic operation is considered an error,
+//! but it is too costly to check everywhere in optimized builds.
+//! For each floating point value that is created, a `debug_assert!` invocation is used
+//! to check if it is valid or not.
+//! This way, there are guarantees when developing code that floating point
+//! numbers have valid values,
+//! but during a release run there is *no overhead* for using these floating
+//! point types compared to using `f32` or `f64` directly.
+//!
+//! This crate makes use of the floating point and number traits in the
+//! popular `num_traits` crate.
+//!
+//! #Examples
+//! An example using the `R64` type, which corresponds to *finite* `f64` values.
+//!
+//! ```
+//! use noisy_float::prelude::*;
+//!
+//! fn geometric_mean(a: R64, b: R64) -> R64 {
+//!     (a * b).sqrt() //used just like regular floating-point numbers
+//! }
+//! 
+//! println!("geometric_mean(10.0, 20.0) = {}", geometric_mean(r64(10.0), r64(20.0)));
+//! //prints 14.142...
+//! ```
+//!
+//! An example using the `N32` type, which corresponds to *non-NaN* `f32` values.
+//! The float types in this crate are able to implement `Eq` and `Ord` properly,
+//! since NaN is not allowed.
+//!
+//! ```
+//! use noisy_float::prelude::*;
+//!
+//! let values = vec![n32(3.0), n32(-1.5), n32(71.3), N32::infinity()];
+//! assert!(values.iter().cloned().min() == Some(n32(-1.5)));
+//! assert!(values.iter().cloned().max() == Some(N32::infinity()));
+//! ```
+
 extern crate num_traits;
 
 pub mod checkers;
 pub mod types;
 mod float_impl;
 
+/// Prelude for the `noisy_float` crate.
+///
+/// This includes all of the types defined in the `noisy_float::types` module,
+/// as well as re-exports of traits from the `num_traits` crate.
+/// It is important to have these re-exports here, because they allow the user
+/// to access common floating point methods like `abs()`, `sqrt()`, etc.
 pub mod prelude {
     pub use types::*;
     
@@ -29,13 +83,21 @@ use std::marker::PhantomData;
 use std::fmt;
 use num_traits::Float;
 
-//FIXME add doc comments
-//FIXME proofread rustdocs, make sure the impls from float_impl are included
-
+/// Trait for checking whether a floating point value is *valid*.
+///
+/// The implementation defines its own criteria for what constitutes a *valid* value.
 pub trait FloatChecker<F> {
-    fn assert(value: F);
+    /// Returns `true` if (and only if) the given floating point value is *valid*
+    /// according to this checker's criteria.  The only hard requirement is that NaN *must* be considered *invalid*
+    /// for all implementations of `FloatChecker`.
     fn check(value: F) -> bool;
+    
+    /// A function that may panic if the floating point value is *invalid*.
+    /// Should either call `assert!(check(value), ...)` or `debug_assert!(check(value), ...)`.
+    fn assert(value: F);
 }
+
+//FIXME: finish documenting
 
 pub struct NoisyFloat<F: Float, C: FloatChecker<F>> {
     value: F,
@@ -76,6 +138,8 @@ impl<F: Float, C: FloatChecker<F>> NoisyFloat<F, C> {
     }
 }
 
+/// Note: due to complications with Rust's type system, cannot implement `Into` generically like
+/// `impl<F: Float, C: FloatChecker<F>> Into<F> for NoisyFloat<F, C>`.
 impl<C: FloatChecker<f32>> Into<f32> for NoisyFloat<f32, C> {
     #[inline]
     fn into(self) -> f32 {
@@ -83,6 +147,8 @@ impl<C: FloatChecker<f32>> Into<f32> for NoisyFloat<f32, C> {
     }
 }
 
+/// Note: due to complications with Rust's type system, cannot implement `Into` generically like
+/// `impl<F: Float, C: FloatChecker<F>> Into<F> for NoisyFloat<F, C>`.
 impl<C: FloatChecker<f64>> Into<f64> for NoisyFloat<f64, C> {
     #[inline]
     fn into(self) -> f64 {
